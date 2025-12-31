5878998a58a1aa7d03a4392230e74e19f449826d
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-31 16:21:39 -0300

    feat: create Client entity

--- src/test/java/com/sebastiao/clientcrudapi/ClientCrudApiApplicationTests.java
+++ src/test/java/com/sebastiao/clientcrudapi/ClientCrudApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClientCrudApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
