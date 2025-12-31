81cea5fe772309acac11d9ed934951f44bc500bf
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-31 14:28:16 -0300

    feat: initial Spring Boot setup

---
+++ src/main/java/com/sebastiao/clientcrudapi/ClientCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiao.clientcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastiao/clientcrudapi/ClientCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiao.clientcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
