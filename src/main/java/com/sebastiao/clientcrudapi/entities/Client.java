6d8405e197fa24e5b250fe17974e88ef6b8342a3
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-31 18:48:12 -0300

    feat: create client service

--- src/main/java/com/sebastiao/clientcrudapi/entities/Client.java
+++ src/main/java/com/sebastiao/clientcrudapi/entities/Client.java
@@ -1,10 +1,16 @@
 package com.sebastiao.clientcrudapi.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
---
+++ src/main/java/com/sebastiao/clientcrudapi/services/ClientService.java
@@ -0,0 +1,74 @@
+package com.sebastiao.clientcrudapi.services;
+
+import com.sebastiao.clientcrudapi.dto.ClientDTO;
+import com.sebastiao.clientcrudapi.entities.Client;
+import com.sebastiao.clientcrudapi.repositories.ClientRepository;
+import com.sebastiao.clientcrudapi.services.exceptions.DatabaseException;
+import com.sebastiao.clientcrudapi.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client client = new Client();
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id,ClientDTO dto){
+        try {
+            Client obj = repository.getReferenceById(id);
+            copyDtoToEntity(dto, obj);
+            obj = repository.save(obj);
+            return new ClientDTO(obj);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if(!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+    }
+}
